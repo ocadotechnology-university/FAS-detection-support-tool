c1021c7306751014350a01d0382277960a1be23c
Krzysztof <krzysztof.mitko@gmail.com>
2024-04-04 20:17:40 +0200

    dodałem dokonywanie pomiarów w mainie. Aktualne flow jest bardzo bazowe - aplikacja konsolowa.
    Input: hardcoded zdjęcie,
    Output: pomiary wyświetlone w konsoli.
    
    Dokonywanie pomiarów jeszcze niezaimplementowane.

--- implementation/main.py
+++ implementation/main.py
@@ -9,13 +9,15 @@ from download.validation.validate_file import FileNotCorrectException
 from download.validation.validate_file_content import ValidateFileContent
 from download.validation.validate_file_content import FileContentNotValidException
 from implementation.processing.measurement import Measurement
-from implementation.processing.measurement_handler import MeasurementsNotCorrect
+from implementation.processing.measurement_handler import MeasurementsNotCorrect, MeasurementHandler
 
 
 def main():
+    global measurement_results
     file = "photo.png"
     validate_file = ValidateFile()
     validate_file_content = ValidateFileContent()
+    measurement_handler = MeasurementHandler()
 
     try:
         validate_file.validate(file)
@@ -23,6 +25,18 @@ def main():
     except FileNotCorrectException:
         # handle exception
         pass
+
+    try:
+        measurement_results = measurement_handler.measure(file)
+        measurement_handler.validate(measurement_results)
+    except MeasurementsNotCorrect:
+        # handle exception
+        pass
+
+    print(f'{measurement_results.right_eye=}')
+    print(f'{measurement_results.left_eye=}')
+    print(f'{measurement_results.philtrum=}')
+    print(f'{measurement_results.lip=}')
 
 
 if __name__ == "__main__":
