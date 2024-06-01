a1b859dfdca478b5b2368e7fd5bbafd3b0e48eb8
Krzysztof <krzysztof.mitko@gmail.com>
2024-06-01 12:04:01 +0200

    i abstracted some methods and I managed to reduce the file length from 151 lines to 97 lines

--- implementation/GUI/scene.py
+++ implementation/GUI/scene.py
@@ -10,141 +10,87 @@ class Scene(qtw.QGraphicsScene):
         super().__init__(*args, **kwargs)
 
         self.reference_points = []
-        self.reference_lines = []
+        self.lines = []
         self.lip_points = []
-        self.lip_line = None
         self.left_eye_points = []
-        self.left_eye_line = None
         self.right_eye_points = []
-        self.right_eye_line = None
 
-        self.radius = 5
+        self.radius = 7
         self.point_pen = qtg.QPen()
-        self.point_pen.setColor(qtg.QColor(0xFF0000))
+        self.point_pen.setColor(qtg.QColor(0xD9D9D9))
         self.point_pen.setWidth(self.radius)
 
-        self.stroke_width = 1
+        self.stroke_width = 7
         self.line_pen = qtg.QPen()
-        self.line_pen.setColor(qtg.QColor(0xFF0000))
+        self.line_pen.setColor(qtg.QColor(0xD9D9D9))
         self.line_pen.setWidth(self.stroke_width)
 
     def reset(self, items):
-        # if a user clicks multiple times "detect reference" we need to make sure we don't draw excessive points
+        # if a user clicks multiple times "detect ..." we need to make sure we don't draw excessive points
         for item in items:
             self.removeItem(item)
         items.clear()
 
-    def draw_reference(self, reference_coords):
-        self.reset(self.reference_points)
-        # draw points
-        for i in range(4):
-            x = reference_coords[i][0]
-            y = reference_coords[i][1]
+    def draw_points(self, points_list, new_coords):
+        self.reset(points_list)
+        for i in range(len(new_coords)):
+            x = new_coords[i][0]
+            y = new_coords[i][1]
             point = Point(x - self.radius, y - self.radius, self.radius * 2, self.radius * 2)
             point.setPen(self.point_pen)
             self.addItem(point)
-            self.reference_points.append(point)
-
+            points_list.append(point)
+            point.setToolTip("XD")
         self.draw_lines()
+
+    def draw_reference(self, reference_coords):
+        self.draw_points(self.reference_points, reference_coords)
+
+    def draw_left_eye(self, coords):
+        self.draw_points(self.left_eye_points, coords)
+
+    def draw_right_eye(self, coords):
+        self.draw_points(self.right_eye_points, coords)
+
+    def draw_upper_lip(self, coords):
+        self.draw_points(self.lip_points, coords)
+
+    def sub_draw_lines(self, point_list):
+        length = len(point_list)
+        for i in range(length):
+            next_i = (i + 1) % length
+            x1 = point_list[i].real_x()
+            y1 = point_list[i].real_y()
+            x2 = point_list[next_i].real_x()
+            y2 = point_list[next_i].real_y()
+
+            line = qtw.QGraphicsLineItem(x1, y1, x2, y2)
+            line.setPen(self.line_pen)
+            self.addItem(line)
+            self.lines.append(line)
 
     def draw_lines(self):
         # reference lines
-        self.reset(self.reference_lines)
-
+        self.reset(self.lines)
         if self.reference_points:
-            for i in range(4):
-                next_i = (i + 1) % 4
-                x1 = self.reference_points[i].real_x()
-                y1 = self.reference_points[i].real_y()
-                x2 = self.reference_points[next_i].real_x()
-                y2 = self.reference_points[next_i].real_y()
-
-                line = qtw.QGraphicsLineItem(x1, y1, x2, y2)
-                line.setPen(self.line_pen)
-                self.addItem(line)
-                self.reference_lines.append(line)
-
-        # left eye
-        if len(self.left_eye_points) == 2:
-            if not (self.left_eye_line is None):
-                self.removeItem(self.left_eye_line)
-            x1 = self.left_eye_points[0].real_x()
-            y1 = self.left_eye_points[0].real_y()
-            x2 = self.left_eye_points[1].real_x()
-            y2 = self.left_eye_points[1].real_y()
-            line = qtw.QGraphicsLineItem(x1, y1, x2, y2)
-            line.setPen(self.line_pen)
-            self.addItem(line)
-            self.left_eye_line = line
-
-        # right eye
-        if len(self.right_eye_points) == 2:
-            if not (self.right_eye_line is None):
-                self.removeItem(self.right_eye_line)
-            x1 = self.right_eye_points[0].real_x()
-            y1 = self.right_eye_points[0].real_y()
-            x2 = self.right_eye_points[1].real_x()
-            y2 = self.right_eye_points[1].real_y()
-            line = qtw.QGraphicsLineItem(x1, y1, x2, y2)
-            line.setPen(self.line_pen)
-            self.addItem(line)
-            self.right_eye_line = line
-
-        # upper lip
-        if len(self.lip_points) == 2:
-            if not (self.lip_line is None):
-                self.removeItem(self.lip_line)
-            x1 = self.lip_points[0].real_x()
-            y1 = self.lip_points[0].real_y()
-            x2 = self.lip_points[1].real_x()
-            y2 = self.lip_points[1].real_y()
-            line = qtw.QGraphicsLineItem(x1, y1, x2, y2)
-            line.setPen(self.line_pen)
-            self.addItem(line)
-            self.lip_line = line
-
-    def draw_left_eye(self, coords):
-        self.reset(self.left_eye_points)
-        # draw points
-        for i in range(2):
-            x = coords[i][0]
-            y = coords[i][1]
-            point = Point(x - self.radius, y - self.radius, self.radius * 2, self.radius * 2)
-            point.setPen(self.point_pen)
-            self.addItem(point)
-            self.left_eye_points.append(point)
-        self.draw_lines()
-
-    def draw_right_eye(self, coords):
-        self.reset(self.right_eye_points)
-        # draw points
-        for i in range(2):
-            x = coords[i][0]
-            y = coords[i][1]
-            point = Point(x - self.radius, y - self.radius, self.radius * 2, self.radius * 2)
-            point.setPen(self.point_pen)
-            self.addItem(point)
-            self.right_eye_points.append(point)
-        self.draw_lines()
-
-    def draw_upper_lip(self, coords):
-        self.reset(self.lip_points)
-        # draw points
-        for i in range(2):
-            x = coords[i][0]
-            y = coords[i][1]
-            point = Point(x - self.radius, y - self.radius, self.radius * 2, self.radius * 2)
-            point.setPen(self.point_pen)
-            self.addItem(point)
-            self.lip_points.append(point)
-        self.draw_lines()
+            self.sub_draw_lines(self.reference_points)
+        if self.left_eye_points:
+            self.sub_draw_lines(self.left_eye_points)
+        if self.right_eye_points:
+            self.sub_draw_lines(self.right_eye_points)
+        if self.lip_points:
+            self.sub_draw_lines(self.lip_points)
 
     def clear_canva_state(self):  # wyczyść kanwę
         self.reference_points = []
-        self.reference_lines = []
+        self.lines = []
         self.lip_points = []
-        self.lip_line = None
         self.left_eye_points = []
-        self.left_eye_line = None
         self.right_eye_points = []
-        self.right_eye_line = None
+
+    # def set_radius_and_stroke(self):
+    #     width = (self.width())
+    #     height = (self.height())
+    #     x = min (width,height)
+    #     self.radius = x * 0.01
+    #     self.stroke_width = x * 0.005
