1abdd0f0fbc68b8d3014bd92047225d9c6d25c79
Krzysztof <krzysztof.mitko@gmail.com>
2024-06-12 11:06:21 +0200

    forgot to update an interface 🤦

--- implementation/raport/generator/raport_generator_interface.py
+++ implementation/raport/generator/raport_generator_interface.py
@@ -5,7 +5,7 @@ class RaportGeneratorInterface:
     """Class for generating raports with centile charts"""
 
     @abstractmethod
-    def generate(self, figures_to_save):
+    def generate(self, figures_to_save, file_path):
         """Generates and saves the raport
         """
         ...
